e5059dd192795e297568a0af7e7ba7c3b6cb7e69
Silas Barta <sbarta@gmail.com>
2015-08-01 19:47:52 -0700

    Only members of product producer can edit.

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,36 @@
+import json
+
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APITestCase
+
+# Using the standard RequestFactory API to create a form POST request
+
+class TestProducts(APITestCase):
+
+    fixtures = ["seed.json"]
+
+    def setUp(self):
+        pass
+
+    def test_product1(self):
+        new_name = "zoggy"
+        changes = {
+            "name": new_name,
+        }
+        response = self.client.patch('/products/1/', changes)
+        self.assertEqual(response.status_code, 403)
+        response = self.client.get('/products/1/')
+        self.assertEqual(response.status_code, 200)
+        self.client.login(username="corp", password="hunter2")
+        response = self.client.get('/products/1/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.patch('/products/1/', changes)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        self.assertEqual(data["name"], new_name)
+        self.client.login(username="noncorp", password="hunter2")
+        response = self.client.patch('/products/1/', changes)
+        self.assertEqual(response.status_code, 403)
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from api.serializers import (UserSerializer, GroupSerializer,
     ProductSerializer)
 
@@ -23,9 +23,27 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
 
 
+# Permissions
+class ProductPermission(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method == "GET":
+            return True
+        else:
+            user = request.user
+            groups = user.groups.all()
+            producer = obj.producer
+            if user.is_authenticated() and producer in groups:
+                return True
+            return False
+
+
 class ProductViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = (ProductPermission, )
+
+
